093e8fc66f04d47cba8534427cd00f328346676d
cquoss <clemens@quoss.de>
2024-12-12 11:40:49 +0100

    Share (faulty) work in progress.

---
+++ jsch-project-upload/src/main/java/com/github/cquoss/jsch/project/upload/App.java
@@ -0,0 +1,52 @@
+package com.github.cquoss.jsch.project.upload;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+public class App {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    // public api methods
+
+    public static void main(final String[] args) {
+        new App().run();
+    } 
+    
+    // private helper methods
+
+    private void run() {
+        final ChannelSftp channel;
+        final JSch jsch = new JSch();
+        final Session session;
+        try {
+            session = jsch.getSession("foo", "localhost", 22);
+        } catch (final JSchException e) {
+            throw new IllegalStateException("Error getting session: " + e.getMessage(), e);
+        }
+        Properties config = new Properties(); 
+        config.put("StrictHostKeyChecking", "no");
+        session.setConfig(config);
+        LOGGER.trace("run [session={}]", session);
+        session.setPassword("bar");
+        try {
+            session.connect();
+        } catch (final JSchException e) {
+            throw new IllegalStateException("Error connecting session: " + e.getMessage(), e);
+        }
+        try {
+             channel = (ChannelSftp) session.openChannel("sftp");
+        } catch (final JSchException e) {
+            throw new IllegalStateException("Error opening channel: " + e.getMessage(), e);
+        }
+        LOGGER.info("run [channel={}]", channel);
+    }
+
+}
